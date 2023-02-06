221fd7084b5fba87ac2bf736e7b116fd41257d82
Grace <74047283+Grace2525@users.noreply.github.com>
2023-02-07 02:00:37 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
+function title(){
+	document.write("
+      <title>Grace25舒服的小窝 - this is a 蒟蒻</title>
+      <link rel="stylesheet" type="text/css" href="../static/base.css"/>
+      <link rel="stylesheet" type="text/css" href="../static/blog.css"/>
+	  <link rel="stylesheet" type="text/css" href="../static/side.css"/>
+	  <link rel="icon" type="img/icon" href="../img/59399592_p0.ico"/>
+	  <script src="https://kit.fontawesome.com/9fc2343d40.js" crossorigin="anonymous"></script>");
+}
+function titleForIndex(){
+	document.write("
+      <title>Grace25舒服的小窝 - this is a 蒟蒻</title>
+      <link rel="stylesheet" type="text/css" href="./static/base.css"/>
+      <link rel="stylesheet" type="text/css" href="./static/blog.css"/>
+	  <link rel="stylesheet" type="text/css" href="./static/side.css"/>
+	  <link rel="icon" type="img/icon" href="./img/59399592_p0.ico"/>
+	  <script src="https://kit.fontawesome.com/9fc2343d40.js" crossorigin="anonymous"></script>");
+}
 function header(){
     document.write("<h2>
       <div class="nav">
