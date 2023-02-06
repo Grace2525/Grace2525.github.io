6ceda06e1b9ce4a86acab7e0e3fe0704c2f60ba9
Grace <74047283+Grace2525@users.noreply.github.com>
2023-02-07 01:51:22 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+function header(){
+    document.write("<h2>
+      <div class="nav">
+        <ul>
+          <li><a href="https://grace2525.github.io/">Grace2525的博客</a><li>
+          <li><a href="https://grace2525.github.io/" style="color:white;">首页  <i class="fa-solid fa-clipboard-list"></i></a></li>
+        </ul>
+      </div>
+    </h2>
+    
+    <br />
+    <br />");
+}
+function footer(){
+    document.write("<div class="footer">
+      <p style="font-size:35px;font-family:consola">Develop by <a href = "https://github.com/Grace2525">Grace2525</a>,2022.06.09</p>
+    </div>");
+}
