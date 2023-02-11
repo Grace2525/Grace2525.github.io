a97dc8436cae5c00fc009921b91a1103eefd02c4
Grace <74047283+Grace2525@users.noreply.github.com>
2023-02-12 05:44:39 +0700

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+function footer(){
+    document.write("<div class="footer">
+      <p style="font-size:35px;font-family:consola">Develop by <a href = "https://github.com/Grace2525">Grace2525</a>,2022.06.09</p>
+    </div>");
+}
+footer();
