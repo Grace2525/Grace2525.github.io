55d5fefb3424dc52e949aff850bc620ad33f89d0
Grace <74047283+Grace2525@users.noreply.github.com>
2023-02-12 05:44:13 +0700

    Create header.js

---
+++ js/header.js
@@ -0,0 +1,14 @@
+function header(){
+    document.write("<h2>
+      <div class="nav">
+        <ul>
+          <li><a href="https://grace2525.github.io/">Grace2525的博客</a><li>
+          <li><a href="https://grace2525.github.io/" style="color:white;">首页  <i class="fa-solid fa-clipboard-list"></i></a></li>
+        </ul>
+      </div>
+    </h2>
+    
+    <br />
+    <br />");
+}
+header();
