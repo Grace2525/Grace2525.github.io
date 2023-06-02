eee7dfbadfbcf7d22d1bb71cf60b162f8469a68f
Grace <74047283+Grace2525@users.noreply.github.com>
2023-06-02 13:04:34 +0800

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -1,6 +1,1 @@
-function footer(){
-    document.write("<div class="footer">
-      <p style="font-size:35px;font-family:consola">Develop by <a href = "https://github.com/Grace2525">Grace2525</a>,2022.06.09</p>
-    </div>");
-}
-footer();
+document.write("<p style="font-size:35px;font-family:consola">Develop by <a href = "https://github.com/Grace2525">Grace2525</a>,2022.06.09</p>");
