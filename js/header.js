9ddc4ff62566a114e1ced1aa1f25045d7dbddab5
Grace <74047283+Grace2525@users.noreply.github.com>
2023-06-02 13:08:21 +0800

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,14 +1,1 @@
-function header(){
-    document.write("<h2>
-      <div class="nav">
-        <ul>
-          <li><a href="https://grace2525.github.io/">Grace2525的博客</a><li>
-          <li><a href="https://grace2525.github.io/" style="color:white;">首页  <i class="fa-solid fa-clipboard-list"></i></a></li>
-        </ul>
-      </div>
-    </h2>
-    
-    <br />
-    <br />");
-}
-header();
+document.write("<ul><li><a href="https://grace2525.github.io/">Grace2525的博客</a></li><li><a href="https://grace2525.github.io/" style="color:white;">首页  <i class="fa-solid fa-clipboard-list"></i></a></li></ul>");
